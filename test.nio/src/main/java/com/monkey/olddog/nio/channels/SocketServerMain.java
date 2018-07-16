bc9a92b406c2212b76068dadab5ac8389e19486c
hanyong <hanyong@xinguangnet.com>
2018-07-16 19:31:45 +0800

    update SocketServerMain.java

--- test.nio/src/main/java/com/monkey/olddog/nio/channels/SocketServerMain.java
+++ test.nio/src/main/java/com/monkey/olddog/nio/channels/SocketServerMain.java
@@ -3,7 +3,9 @@ package com.monkey.olddog.nio.channels;/**
  */
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 /**
  * @author hanyong
@@ -12,8 +14,22 @@ import java.nio.channels.ServerSocketChannel;
 public class SocketServerMain {
 
     public static void main(String[] args) {
-        try (ServerSocketChannel channel = ServerSocketChannel.open()){
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()){
+//            channel.configureBlocking(false);
 
+            serverSocketChannel.bind(new InetSocketAddress(8800));
+
+            while (true) {
+
+                SocketChannel channel = serverSocketChannel.accept();
+
+//                channel.
+                // todo
+
+
+
+
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
