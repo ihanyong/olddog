4d1ebb8525d78220bf5039e3400780bc23fefc46
韩勇 <hanyong@xinguangnet.com>
2018-09-12 21:49:46 +0800

    update

--- leetcode/src/main/java/com/monkey/olddog/leetcode/singleNumber/Solution.java
+++ leetcode/src/main/java/com/monkey/olddog/leetcode/singleNumber/Solution.java
@@ -7,4 +7,21 @@ package com.monkey.olddog.leetcode.singleNumber;/**
  * @Date 2018/9/6
  */
 public class Solution {
+    public int singleNumber(int[] nums) {
+        int r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            r ^= nums[i];
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(5 ^ 5 ^ 3 ^ 3);
+        System.out.println(5 ^ 0);
+        System.out.println(5 ^ 1);
+        System.out.println(5 ^ 2);
+        System.out.println(5 ^ 3);
+        System.out.println(5 ^ 4);
+    }
+
 }
